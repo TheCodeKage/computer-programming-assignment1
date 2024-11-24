6597e301bf7cf0cf2121c1bc2602ac495a2e9c6e
Acer <Nam.agr96@gmail.com>
2024-11-24 15:05:43 +0530

    Finished dictionary questions

--- coding-assignment/dict/dict_q12.py
+++ coding-assignment/dict/dict_q12.py
@@ -5,15 +5,30 @@ rearranging remaining characters.
 
 Input : s1 = ABHISHEKsinGH
       : s2 = gfhfBHkooIHnfndSHEKsiAnG
-Output : Possible
+Output : True
 
 Input : s1 = Hello
       : s2 = dnaKfhelddf
-Output : Not Possible
+Output : False
 
 Input : s1 = GeeksforGeeks
       : s2 = rteksfoGrdsskGeggehes
-Output : Possible
+Output : True
 """
+from collections import Counter
 
-def make_string(shorter: str, )
+
+def make_string(shorter: str, longer:str) -> bool:
+    """
+    :param shorter: The string that's to be made from the longer one
+    :param longer:  The sting whose characters we will rearrange and delete to make the shorter one
+    :return: True if we can make the shorter string from longer, else False
+    """
+    s1 = Counter(shorter)
+    s2 = Counter(longer)
+    for i in s1.keys():
+        if i not in s2.keys() or s1[i] > s2[i]:
+            return False
+    return True
+
+print(make_string('GeeksforGeeks', 'rteksfoGrdsskGeggehes'))
