c5b9fb22ecf04d17f25d18b1d86c6af350e6f9df
Acer <Nam.agr96@gmail.com>
2024-11-24 13:55:33 +0530

    Added all necessary files and completed all questions I could.

---
+++ coding-assignment/dict/dict_q12.py
@@ -0,0 +1,19 @@
+"""
+@geeksforgeeks
+Given two strings, find if we can make first string from second by deleting some characters from second and
+rearranging remaining characters.
+
+Input : s1 = ABHISHEKsinGH
+      : s2 = gfhfBHkooIHnfndSHEKsiAnG
+Output : Possible
+
+Input : s1 = Hello
+      : s2 = dnaKfhelddf
+Output : Not Possible
+
+Input : s1 = GeeksforGeeks
+      : s2 = rteksfoGrdsskGeggehes
+Output : Possible
+"""
+
+def make_string(shorter: str, )
